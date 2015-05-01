1a40ea8a993035e29efe37a934ac380544231687
Tony van Riet <tonyvanriet@gmail.com>
2015-04-30 22:45:05 -0500

    update project version and depencencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-slack-client "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-slack-client "0.1.1-SNAPSHOT"
+  :description "A client for Slack's RTM and Web APIs"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.4.0"]
-                 [aleph "0.4.0-beta2"]
-                 [byte-streams "0.2.0-alpha3"]])
+                 [aleph "0.4.0"]
+                 [byte-streams "0.2.0"]])
