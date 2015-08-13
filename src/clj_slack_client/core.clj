7405f8553c31960b31d3b31fa240fee40cf15669
Martin Janiczek <martin@janiczek.cz>
2015-08-13 21:21:36 +0200

    + add options map (for now to be able to turn off logging)

--- src/clj_slack_client/core.clj
+++ src/clj_slack_client/core.clj
@@ -15,17 +15,20 @@
 
 
 (defn connect
-  [api-token host-handle-event]
-  (let [rx-event-stream (stream/stream 8)
-        pass-event-to-rx #(stream/put! rx-event-stream %)
-        host-event-stream (stream/stream 8)
-        pass-event-to-host #(stream/put! host-event-stream %)
-        reconnect (fn [] (do (println "reconnecting...")
-                             (disconnect)
-                             (connect api-token host-handle-event)))]
-    (stream/consume #(rx/handle-event % pass-event-to-host) rx-event-stream)
-    (stream/consume host-handle-event host-event-stream)
-    (conn/start-real-time api-token state/set-team-state pass-event-to-rx reconnect)))
+  ([api-token host-handle-event]
+   (connect api-token host-handle-event {:log true}))
+  ([api-token host-handle-event options]
+   (let [rx-event-stream (stream/stream 8)
+         pass-event-to-rx #(stream/put! rx-event-stream %)
+         host-event-stream (stream/stream 8)
+         pass-event-to-host #(stream/put! host-event-stream %)
+         reconnect (fn [] (do (when (options :log)
+                                (println "reconnecting..."))
+                              (disconnect)
+                              (connect api-token host-handle-event)))]
+     (stream/consume #(rx/handle-event % pass-event-to-host) rx-event-stream)
+     (stream/consume host-handle-event host-event-stream)
+     (conn/start-real-time api-token state/set-team-state pass-event-to-rx reconnect options))))
 
 
 (defn time->ts
