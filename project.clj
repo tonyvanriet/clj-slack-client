6d47ccdf476fda97a644c65977b163d465129638
Tony van Riet <tonyvanriet@gmail.com>
2016-04-14 00:55:12 -0500

    Bump version to 0.1.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-slack-client "0.1.5-SNAPSHOT"
+(defproject clj-slack-client "0.1.6-SNAPSHOT"
   :description "A client for Slack's RTM and Web APIs"
   :url ""
   :license {:name "Eclipse Public License"
