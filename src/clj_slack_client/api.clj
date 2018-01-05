4b2c2a1ab49977ed65523802599c3dd30d7fc88c
Michael Davis <rubiksmaster65@yahoo.com>
2018-01-04 22:12:53 -0500

    Implement the sole api test method

---
+++ src/clj_slack_client/api.clj
@@ -0,0 +1,11 @@
+(ns clj-slack-client.api
+  (:reqire [clj-slack-client.web :as web])
+  (:refer-clojure :exclude [test]))
+
+
+(defn test
+  "Checks API calling code. Callable by workspace and user tokens."
+  ([]
+   (web/call-and-get-response "api.test"))
+  ([options]
+   (web/call-and-get-response "apt.test" options)))
